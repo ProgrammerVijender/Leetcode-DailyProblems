8dc2d58326388ac6f14ca9953d297708f84d6776
Vijender <65845843+ProgrammerVijender@users.noreply.github.com>
2025-12-18 15:55:46 +0530

    Time: 34 ms (80.91%), Space: 11.2 MB (21.71%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -21,35 +21,69 @@ public:
     // }
 
     // recursion + memoisation 
-    int solUsingMemoisation(int start , int end , vector<vector<int>> &dp)
-    {
-        // base case
-        if(start >= end)
-        {
-            return 0;
-        }
-        if(dp[start][end] != -1)
-        {
-            return dp[start][end];
-        }
+    // int solUsingMemoisation(int start , int end , vector<vector<int>> &dp)
+    // {
+    //     // base case
+    //     if(start >= end)
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[start][end] != -1)
+    //     {
+    //         return dp[start][end];
+    //     }
 
-        int ans = INT_MAX;
+        // int ans = INT_MAX;
+
+        // for(int i=start; i<=end; i++ )
+        // {
+        //     ans = min(ans , i+ max( solUsingMemoisation(start , i-1 , dp) , solUsingMemoisation(i+1 , end ,dp)));
+        // }
+    //     dp[start][end] = ans;
+    //     return dp[start][end];
+    // }
+
+
+    // Solve usinng Tabulation Method
+    int solvingUsingTabulation(int n)
+    {
+        vector<vector<int> >  dp(n+2, vector<int>(n+2, 0));
+        
+        for(int start=n; start >=1; start--)
+        {
+            for(int end=1; end<=n; end++ )
+            {
+                if (start >= end) {
+    dp[start][end] = 0;
+    continue;
+}
+                int ans = INT_MAX;
 
         for(int i=start; i<=end; i++ )
         {
-            ans = min(ans , i+ max( solUsingMemoisation(start , i-1 , dp) , solUsingMemoisation(i+1 , end ,dp)));
+            ans = min(ans , i+ max( dp[start][ i-1 ] , dp[i+1 ][end]));
         }
+        
         dp[start][end] = ans;
-        return dp[start][end];
+            }
+
+        }
+
+    return dp[1][n];
     }
 
     int getMoneyAmount(int n) {
-        int start = 1;
-        int end  = n;
+        // int start = 1;
+        // int end  = n;
 
-        vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
+        // memoisation :--
+        // vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
 
-        int ans = solUsingMemoisation(start , end , dp);
-        return ans;
+        // int ans = solUsingMemoisation(start , end , dp);
+
+        // Tabulation method :--
+        return solvingUsingTabulation(n);
+
+
     }
 };
