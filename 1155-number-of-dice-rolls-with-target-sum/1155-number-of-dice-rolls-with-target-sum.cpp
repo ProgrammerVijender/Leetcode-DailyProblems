2e6d53a0852c2617bedc8adb0e721301bb318544
Vijender <65845843+ProgrammerVijender@users.noreply.github.com>
2025-12-19 16:11:20 +0530

    Time: 15 ms (48.11%), Space: 11.2 MB (68.07%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,77 +1,79 @@
 class Solution {
 public:
+    long long int MOD = 1000000007;
     // recursion :--
-    // int solveUsingRecusrion(int n, int k, int target)
-    // {
-        
-    //     if(n== 0 && target == 0)
-    //     {
+    // int solveUsingRecursion(int n, int k, int target,
+    //                         int diceUsed, int currSum) {
+
+    //     if (diceUsed == n && currSum == target) {
     //         return 1;
     //     }
 
-    //     if(n != 0 && target == 0)
-    //     {
+    //     if (diceUsed == n && currSum != target) {
     //         return 0;
     //     }
 
-    //     if(n == 0 && target != 0)
-    //     {
+    //     if (currSum == target && diceUsed != n) {
     //         return 0;
     //     }
-        
-        
+
     //     int ans = 0;
-
-    //     for(int i=0; i<=k; i++)
-    //     {
-    //         ans += solveUsingRecusrion(n-1, k,target - i);
+    //     for (int face = 1; face <= k; face++) {
+    //         ans = ans + solveUsingRecursion(n, k, target,diceUsed + 1,currSum + face);
     //     }
+
     //     return ans;
     // }
 
-    // memoisation :---
-    long long int MOD = 1000000007;
-    int solveUsingMemoisation(int n, int k, int target , vector<vector<int> >& dp)
-    {
-        if (target < 0)
-        {return 0;}
-        
-        if(n== 0 && target == 0)
-        {
-            return 1;
-        }
+    // Memoisation :---
 
-        if(n != 0 && target == 0)
-        {
-            return 0;
-        }
+    int solveUsingMemo(int n, int k, int target,
+                  int diceUsed, int currSum,
+                  vector<vector<int>>& dp) {
 
-        if(n == 0 && target != 0)
-        {
-            return 0;
-        }
-
-        if(dp[n][target] != -1)
-        {
-            return dp[n][target];
-        }
-        
-        
-        int ans = 0;
-
-        for(int i=1; i<=k; i++)
-        {
-             ans = (ans + solveUsingMemoisation(n - 1, k, target - i, dp)) % MOD;
-        }
-        return dp[n][target] = ans;
+    if (diceUsed == n && currSum == target) {
+        return 1;
     }
 
+    if (diceUsed == n && currSum != target) {
+        return 0;
+    }
 
+    if (currSum == target && diceUsed != n) {
+        return 0;
+    }
+
+    if (dp[diceUsed][currSum] != -1) {
+        return dp[diceUsed][currSum];
+    }
+
+    int ans = 0;
+    for (int face = 1; face <= k; face++) {
+        int recAns = 0;
+        if (currSum + face <= target) {
+            recAns = solveUsingMemo(
+                        n, k, target,
+                        diceUsed + 1,
+                        currSum + face,
+                        dp);
+        }
+        ans = (ans % MOD+ recAns% MOD)% MOD;
+    }
+
+    return dp[diceUsed][currSum] = ans;
+}
+
+    
     int numRollsToTarget(int n, int k, int target) {
-        // return solveUsingRecusrion(n , k , target);
+        int diceUsed = 0;
+        int currSum = 0;
+        // recursion
+        // int ans = solveUsingRecursion(n, k, target,diceUsed, currSum);
 
-        vector<vector<int>> dp(n+1,vector<int>(target+1 , -1));
-        return solveUsingMemoisation( n ,k , target , dp);
+        // memoisation
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
+        int ans = solveUsingMemo(n, k, target,diceUsed, currSum , dp);
 
+        return ans;
     }
 };
