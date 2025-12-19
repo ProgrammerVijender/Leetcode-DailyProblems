177847da58a4e5ae2b0662e48b171eb8adf8ee47
Vijender <65845843+ProgrammerVijender@users.noreply.github.com>
2025-12-19 16:25:31 +0530

    Time: 33 ms (6.51%), Space: 9.4 MB (87.9%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -26,42 +26,105 @@ public:
     // }
 
     // Memoisation :---
+//     int solveUsingMemo(int n, int k, int target,
+//                   int diceUsed, int currSum,
+//                   vector<vector<int>>& dp) {
 
-    int solveUsingMemo(int n, int k, int target,
-                  int diceUsed, int currSum,
-                  vector<vector<int>>& dp) {
+//     if (diceUsed == n && currSum == target) {
+//         return 1;
+//     }
 
-    if (diceUsed == n && currSum == target) {
-        return 1;
-    }
+//     if (diceUsed == n && currSum != target) {
+//         return 0;
+//     }
 
-    if (diceUsed == n && currSum != target) {
-        return 0;
-    }
+//     if (currSum == target && diceUsed != n) {
+//         return 0;
+//     }
 
-    if (currSum == target && diceUsed != n) {
-        return 0;
-    }
+//     if (dp[diceUsed][currSum] != -1) {
+//         return dp[diceUsed][currSum];
+//     }
 
-    if (dp[diceUsed][currSum] != -1) {
-        return dp[diceUsed][currSum];
-    }
+//     int ans = 0;
+//     for (int face = 1; face <= k; face++) {
+//         int recAns = 0;
+//         if (currSum + face <= target) {
+//             recAns = solveUsingMemo(
+//                         n, k, target,
+//                         diceUsed + 1,
+//                         currSum + face,
+//                         dp);
+//         }
+//         ans = (ans % MOD+ recAns% MOD)% MOD;
+//     }
 
-    int ans = 0;
-    for (int face = 1; face <= k; face++) {
-        int recAns = 0;
-        if (currSum + face <= target) {
-            recAns = solveUsingMemo(
-                        n, k, target,
-                        diceUsed + 1,
-                        currSum + face,
-                        dp);
+//     return dp[diceUsed][currSum] = ans;
+// }
+    
+
+    // Tabulation Method :-----
+    // int solveUsingTabulation(int n, int k, int target) {
+
+    //     vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+
+    //     // base case
+    //     dp[n][target] = 1;
+
+    //     for (int dice = n - 1; dice >= 0; dice--) {
+    //         for (int sum = target; sum >= 0; sum--) {
+
+    //             int ans = 0;
+
+    //             for (int face = 1; face <= k; face++) {
+    //                 int recAns = 0;
+
+    //                 if (sum + face <= target) {
+    //                     recAns = dp[dice + 1][sum + face];
+    //                 }
+
+    //                 ans = (ans % MOD + recAns % MOD) % MOD;
+    //             }
+
+    //             dp[dice][sum] = ans;
+    //         }
+    //     }
+
+    //     return dp[0][0];
+    // }
+    
+    int solveUsingTabulationSO(int n, int k, int target)
+    {
+         vector<int> curr(target + 1, 0);
+        vector<int> next(target + 1, 0);
+
+        // base case
+        next[target] = 1;
+
+        for (int dice = n - 1; dice >= 0; dice--) {
+
+            for (int sum = target; sum >= 0; sum--) {
+
+                int ans = 0;
+
+                for (int face = 1; face <= k; face++) {
+                    int recAns = 0;
+
+                    if (sum + face <= target) {
+                        recAns = next[sum + face];
+                    }
+
+                    ans = (ans % MOD + recAns % MOD) % MOD;
+                }
+
+                curr[sum] = ans;
+            }
+
+            next = curr;
         }
-        ans = (ans % MOD+ recAns% MOD)% MOD;
-    }
 
-    return dp[diceUsed][currSum] = ans;
-}
+        return next[0];
+    }
 
     
     int numRollsToTarget(int n, int k, int target) {
@@ -71,9 +134,16 @@ public:
         // int ans = solveUsingRecursion(n, k, target,diceUsed, currSum);
 
         // memoisation
-        vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
-        int ans = solveUsingMemo(n, k, target,diceUsed, currSum , dp);
+        // vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
+        // int ans = solveUsingMemo(n, k, target,diceUsed, currSum , dp);
 
-        return ans;
+
+        // TABULATION 
+        // return solveUsingTabulation(n,k,target);
+
+        // tabulation +Space optimised
+        return solveUsingTabulationSO(n,k,target);
+
+        // return ans;
     }
 };
